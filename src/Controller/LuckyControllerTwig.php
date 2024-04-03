798b339a7460c25ef607c536c43a98e53dc58c8a
lucation <lucasdanielssonvalladares@gmail.com>
2024-04-02 21:44:40 -0500

    First commit

---
+++ src/Controller/LuckyControllerTwig.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyControllerTwig extends AbstractController
+{
+    #[Route("/lucky", name: "lucky_number")]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        if ($number > 50) {
+            $foto = 'bild1.jpg';
+        } else {
+            $foto = 'bild2.jpg';
+        }
+
+        $data = [
+            'number' => $number,
+            'foto' => $foto
+        ];
+
+        return $this->render('lucky_number.html.twig', $data);
+    }
+
+    #[Route("/", name: "index")]
+    public function index(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+    #[Route("/report", name: "report")]
+    public function report(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+
+}
+
